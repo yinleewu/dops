3c935e37c5b84fbb3058bf2f9fb5b9cbad45986d
yinleewu <67744994+yinleewu@users.noreply.github.com>
2020-07-02 09:25:19 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+window.onload = function () {
+       
+  if (localStorage.getItem('tems')) {
+    $('.notice').addClass('notice-destroy');
+  } else {
+    $('.notice').removeClass('notice-destroy');
+  }
+
+  phone();
+
+  function phone() {
+    if (phoneBack == false) {
+      $('.mobile').css('left', '30%');
+      $('.name2').addClass('name-active');
+      $('.arrow2').addClass('arrow-active');
+      setTimeout(function () {
+        $('.names').removeClass('name-active');
+        $('.arrows').removeClass('arrow-active');
+        phoneBack = true;
+        phone();
+      }, 2000);
+    } else {
+      $('.name1').addClass('name-active');
+      $('.arrow1').addClass('arrow-active');
+      $('.mobile').css('left', '10%');
+      setTimeout(function () {
+        $('.names').removeClass('name-active');
+        $('.arrows').removeClass('arrow-active');
+        phoneBack = false;
+        phone();
+      }, 2000);
+    }
+  }
+
+  $('.btn-true').on('click', function (e) {
+    e.preventDefault();
+    destroyNotice();
+    localStorage.setItem('tems', 'true');
+  });
+  $('.btn-false').on('click', function (e) {
+    e.preventDefault();
+    destroyNotice(); //localStorage.setItem('tems','false');
+  });
+
+  function destroyNotice() {
+    $('.notice').addClass('notice-destroy');
+  }
+};
